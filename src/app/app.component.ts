06d3593137517e951e8946bad92df11f24ec0fb7
dongre17 <anshuldongre2017@hotmail.com>
2021-02-23 09:46:10 +0530

    Updated changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+import { birthdays, workAnniversaries } from './models/data.interface';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +9,11 @@ export interface Tile {
 export class AppComponent {
 
   panelOpenState = false;
-    
+
   title = 'ribbon-bulletin';
-  tiles: Tile[] = [
-    { text: 'One', cols: 3, rows: 1, color: 'lightblue' },
-    { text: 'Two', cols: 1, rows: 2, color: 'lightgreen' },
-    { text: 'Three', cols: 1, rows: 1, color: 'lightpink' },
-    { text: 'Four', cols: 2, rows: 1, color: '#DDBDF1' },
-  ];
+
+  birthdays = birthdays;
+
+  workAnniversaries = workAnniversaries;
 
 }
---
+++ src/app/models/data.interface.ts
@@ -0,0 +1,20 @@
+export interface IData {
+    Name: string;
+    Date: Date;
+}
+
+export const birthdays: IData[] = [
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')}
+];
+
+export const workAnniversaries: IData[] = [
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')},
+    { Name : 'RanVijay', Date: new Date('2/25/2021')}
+];
